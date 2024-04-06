9d11f4eb9c85bfd7906f80e778bbb0e200e3ccef
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2024-04-05 21:11:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+      USER
+ID (Chave Primária)
+Nome
+Email
+Senha (recomenda-se armazenar um hash da senha)
+
+      PRODUTOS
+ID (Chave Primária)
+Nome
+Descrição
+Preço
+Quantidade em estoque
+
+*/
