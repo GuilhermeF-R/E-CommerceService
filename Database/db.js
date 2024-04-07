0e789c8ea901b7bcaa90ca6f8e6a1cf503e969e6
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2024-04-07 00:49:34 -0300

    password altered

--- Database/db.js
+++ Database/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Carol007',
+  password: '123',
   database: 'ecommerce_data'
 });
 
